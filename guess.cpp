61c988450f0d43485271eef58a7a1ad07cfbbf93
JohnNCrosby <jcrosby7@mail.csuchico.edu>
2017-09-20 12:46:44 -0700

    Still have the library issue but have made it so that the easy mode works

--- guess.cpp
+++ guess.cpp
@@ -6,7 +6,32 @@
 
 int main(){
     srand ( time(NULL) );
-    int randomNumber;
+    int randomNumber, guess;
+    std::string choice;
+    std::cout<<"Easy gives you 1 guess between 1 and 2. \n"<<"Medium gives you two guesses between 1 and 10. \n"<<"Hard gives you 3 guesses between 1 and 100"<<std::endl;
+    std::cout<<"Would you like to do the easy, medium, or hard mode."<<std::endl;
+    std::cin>>choice;
+
+    if(choice == "easy" || choice == "Easy"){
+        randomNumber = rand() % 2 + 1;
+        std::cout<<"Please choose either 1 or 2"<<std::endl;
+        std::cin>>guess;
+        if(guess == randomNumber)
+        {
+            std::cout<<"You win!"<<std::endl;
+        }
+        else
+        {
+            std::cout<<"Sorry it was "<<randomNumber<<"."<<std::endl;
+        }
+    }
+
+
+
+    
+    
+    
+    
     randomNumber = rand() % 10 + 1;
     std::cout<<randomNumber<<std::endl;
 
@@ -18,5 +43,5 @@ int main(){
 
 
 
-
+    return 0;
 }
