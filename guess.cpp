30fd558b978ddb3d0a0f0fbe29a4ae1f24c4ad4d
JohnNCrosby <jcrosby7@mail.csuchico.edu>
2017-09-20 12:15:01 -0700

    Got the random number working though having issues with the iostream not working resulting in having to use std::

---
+++ guess.cpp
@@ -0,0 +1,22 @@
+//Author: John Crosby
+#include<iostream>
+#include<string>
+#include<cstdlib>
+#include<ctime>
+
+int main(){
+    srand ( time(NULL) );
+    int randomNumber;
+    randomNumber = rand() % 10 + 1;
+    std::cout<<randomNumber<<std::endl;
+
+
+
+
+
+
+
+
+
+
+}
