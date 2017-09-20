fede6af4e49e055dfe6563d43b5c099da6c46a9d
JohnNCrosby <jcrosby7@mail.csuchico.edu>
2017-09-20 13:21:54 -0700

    This adds the medium difficulty with the library problem still implemented

--- guess.cpp
+++ guess.cpp
@@ -12,7 +12,8 @@ int main(){
     std::cout<<"Would you like to do the easy, medium, or hard mode."<<std::endl;
     std::cin>>choice;
 
-    if(choice == "easy" || choice == "Easy"){
+    if(choice == "easy" || choice == "Easy")
+    {
         randomNumber = rand() % 2 + 1;
         std::cout<<"Please choose either 1 or 2"<<std::endl;
         std::cin>>guess;
@@ -25,15 +26,43 @@ int main(){
             std::cout<<"Sorry it was "<<randomNumber<<"."<<std::endl;
         }
     }
+    else if(choice == "medium" || choice == "Medium")
+    {
+        randomNumber = rand() % 10 + 1;
+        for(int count = 0; count < 2; count++)
+        {
+            std::cout<<"Please enter a number between 1 and 10."<<std::endl;
+            std::cin>>guess;
+            if(guess == randomNumber)
+            {
+                std::cout<<"You won on attempt "<<count+1<<"!"<<std::endl;
+                count = 2;
+            }
+            else if(guess != randomNumber && count < 1)
+            {
+                if(guess < randomNumber)
+                {
+                    std::cout<<"Sorry the number is higher."<<std::endl;
+                }
+                else
+                {
+                    std::cout<<"Sorry the number is lower."<<std::endl;
+                }
+            }
+            else
+            {
+                std::cout<<"The number was "<<randomNumber<<"."<<std::endl;
+            }
+        }
+    }    
 
 
+    
+    
+    
+    
+    
 
-    
-    
-    
-    
-    randomNumber = rand() % 10 + 1;
-    std::cout<<randomNumber<<std::endl;
 
 
 
