efaf2cd15f701fc3060070dc29f9847c8e9cf937
JohnNCrosby <jcrosby7@mail.csuchico.edu>
2017-09-22 12:41:36 -0700

    Figured out I wasn't useing namespace std. Completed the hard mode challenge. Added the prompt at the end to see if the user wanted to play again or not.

--- guess.cpp
+++ guess.cpp
@@ -3,60 +3,97 @@
 #include<string>
 #include<cstdlib>
 #include<ctime>
+using namespace std;
 
 int main(){
     srand ( time(NULL) );
     int randomNumber, guess;
-    std::string choice;
-    std::cout<<"Easy gives you 1 guess between 1 and 2. \n"<<"Medium gives you two guesses between 1 and 10. \n"<<"Hard gives you 3 guesses between 1 and 100"<<std::endl;
-    std::cout<<"Would you like to do the easy, medium, or hard mode."<<std::endl;
-    std::cin>>choice;
+    string choice, answer;
+    do
+    {
+        cout<<"Easy gives you 1 guess between 1 and 2. \n"<<"Medium gives you two guesses between 1 and 10. \n"<<"Hard gives you 3 guesses between 1 and 100"<<endl;
+        cout<<"Would you like to do the easy, medium, or hard mode."<<endl;
+        cin>>choice;
 
-    if(choice == "easy" || choice == "Easy")
-    {
-        randomNumber = rand() % 2 + 1;
-        std::cout<<"Please choose either 1 or 2"<<std::endl;
-        std::cin>>guess;
-        if(guess == randomNumber)
+        if(choice == "easy" || choice == "Easy")
         {
-            std::cout<<"You win!"<<std::endl;
-        }
-        else
-        {
-            std::cout<<"Sorry it was "<<randomNumber<<"."<<std::endl;
-        }
-    }
-    else if(choice == "medium" || choice == "Medium")
-    {
-        randomNumber = rand() % 10 + 1;
-        for(int count = 0; count < 2; count++)
-        {
-            std::cout<<"Please enter a number between 1 and 10."<<std::endl;
-            std::cin>>guess;
+            cout<<"You have selected easy mode."<<endl;
+            randomNumber = rand() % 2 + 1;
+            cout<<"Please choose either 1 or 2"<<endl;
+            cin>>guess;
             if(guess == randomNumber)
             {
-                std::cout<<"You won on attempt "<<count+1<<"!"<<std::endl;
-                count = 2;
-            }
-            else if(guess != randomNumber && count < 1)
-            {
-                if(guess < randomNumber)
-                {
-                    std::cout<<"Sorry the number is higher."<<std::endl;
-                }
-                else
-                {
-                    std::cout<<"Sorry the number is lower."<<std::endl;
-                }
+                cout<<"You win!"<<std::endl;
             }
             else
             {
-                std::cout<<"The number was "<<randomNumber<<"."<<std::endl;
+                cout<<"Sorry it was "<<randomNumber<<"."<<endl;
             }
         }
-    }    
-
-
+        else if(choice == "medium" || choice == "Medium")
+        {
+            cout<<"You have selected medium difficulty."<<endl;
+            randomNumber = rand() % 10 + 1;
+            for(int count = 0; count < 2; count++)
+            {
+                cout<<"Please enter a number between 1 and 10."<<endl;
+                cin>>guess;
+                if(guess == randomNumber)
+                {
+                    cout<<"You won on attempt "<<count+1<<"!"<<endl;
+                    count = 2;
+                }
+                else if(guess != randomNumber && count < 1)
+                {
+                    if(guess < randomNumber)
+                    {
+                        cout<<"Sorry the number is higher."<<endl;
+                    }
+                    else
+                    {
+                        cout<<"Sorry the number is lower."<<endl;
+                    }
+                }
+                else
+                {
+                    cout<<"The number was "<<randomNumber<<"."<<endl;
+                }
+            }
+        }
+        else
+        {
+            cout<<"Whether you wanted to or not you in hard mode now!"<<endl;
+            randomNumber = rand() % 100 + 1;
+            for(int count = 0; count < 3; count ++)
+            {
+                cout<<"Please enter a number between 1 and 100."<<endl;
+                cin>>guess;
+                if(guess == randomNumber)
+                {
+                    cout<<"You have won on attempt "<<count+1<<"!"<<endl;
+                    count = 3;
+                }
+                else if(guess != randomNumber && count < 2)
+                {
+                    if(guess < randomNumber)
+                    {
+                        cout<<"Sorry the number is higher."<<endl;
+                    }
+                    else
+                    {
+                        cout<<"Sorry the number is lower."<<endl;
+                    }
+                }
+                else
+                {
+                    cout<<"Sorry you are out of attempts. The number was "<<randomNumber<<"."<<endl;
+                }
+            }
+        } 
+        cout<<"Do you still want to play? (yes or no?)"<<endl;
+        cin>>answer;
+    } while(answer == "yes" || answer == "Yes" || answer == "y");
+    cout<<"Thanks for playing!"<<endl;
     
     
     
